339d1f6b32471555d94e6a561c3e631605bc732f
GQSM <ms314006@hotmail.com>
2019-08-30 21:07:35 +0800

    [CHANGE] 調整 Eslint 的設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     },
     parserOptions: {
       ecmaFeatures: {
-        jsx: true,
+          jsx: true,
+          modules: true
       },
       ecmaVersion: 2018,
       sourceType: 'module',
